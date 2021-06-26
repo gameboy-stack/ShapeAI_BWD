b2e22c685f54634a59ff900f13a7bcf3981b8eea
gameboy-stack <gt173211@gmail.com>
2021-06-26 23:40:47 +0530

    finsihed

---
+++ src/components/Info.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function Info(){
+    return(
+    <div className="note">
+
+        <h1>Javascript And React.js</h1>
+        
+        <p>A Basic Web Development React.js Bootcamp</p>
+
+    </div>
+    );
+}
+
+export default Info;
